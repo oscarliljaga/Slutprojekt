7041777c385dba44b4765a58b56e64872796c504
oscarliljaga <oscar.lilja@elev.ga.ntig.se>
2024-03-15 13:33:40 +0100

    Påbörjat klasschema

--- src/Main.java
+++ src/Main.java
@@ -3,3 +3,64 @@ public class Main {
         System.out.println("Hello world!");
     }
 }
+
+/*
+abstract PublicEntity
+    #name: String
+    #url: String
+
+    +getName(): String
+    +getURL(): String
+
+abstract Artist extends PublicEntity
+    #songs: ArrayList<Song>
+    #releases: ArrayList<Release>
+
+    +getSongs(): ArrayList<Song>
+    +getReleases(): ArrayList<Release>
+
+
+SoloArtist extends Artist
+
+
+GroupArtist extends Artist
+    -members: ArrayList<SoloArtist>
+
+    +getMembers: ArrayList<SoloArtist>
+    -addMember(SoloArtist): void
+    -removeMember(SoloArtist): void
+
+
+Song extends PublicEntity
+    -artists: Artist[]
+    -releases: ArrayList<Release>
+
+    Song(name: String, url: String, artists: Artist[])
+    +getArtists(): Artist[]
+    +getReleases(): ArrayList<Release>
+
+
+Release extends PublicEntity
+    -artists: Artist[]
+    -songs: Song[]
+
+    Release(name: String, url: String, artists: Artist[], songs: Song[]) //Adds each song, and this, to each Artist
+    +getArtists(): Artist[]
+    +getSongs(): Song[]
+
+
+Playlist extends PublicEntity
+    -user: User
+    -isPublic: Boolean
+    -songs: ArrayList<Song>
+
+    +getUser(): User
+    -setPublic(boolean): void
+    +getSongs(): ArrayList<Song> //Only if (current user == this.user || public)
+
+
+User extends PublicEntity
+    -playlists: ArrayList<Playlist>
+
+    +getPlaylists(): Playlist[] //Only return public Playlist
+*/
